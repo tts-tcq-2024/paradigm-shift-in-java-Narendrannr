b83ccc3c1c653edc5318beca0c277bd80efc5e2b
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-07-08 04:37:26 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,23 +1,33 @@
 package vitals;
+/**
+* @author AFF1COB
+*
+*/
 
 public class Main {
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            System.out.println("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
+       static boolean isTemperatureOk(float temperature) {
+        return temperature >= 0 && temperature <= 45;
+    }
+
+    static boolean isSocOk(float soc) {
+        return soc >= 20 && soc <= 80;
+    }
+
+    static boolean isChargeRateOk(float chargeRate) {
+        return chargeRate <= 0.8;
+    }
+
+    static boolean isBatteryOk(float temperature, float soc, float chargeRate) {
+        return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
     }
 
     public static void main(String[] args) {
         assert(batteryIsOk(25, 70, 0.7f) == true);
         assert(batteryIsOk(50, 85, 0.0f) == false);
-        System.out.println("Some more tests needed");
+        assert (isBatteryOk(0, 50, 0.7f) == false);  
+        assert (isBatteryOk(45, 50, 0.7f) == false);  
+        assert (isBatteryOk(25, 15, 0.7f) == false);  
+        assert (isBatteryOk(25, 85, 0.7f) == false);  
+        assert (isBatteryOk(25, 50, 1.0f) == false);
     }
 }
