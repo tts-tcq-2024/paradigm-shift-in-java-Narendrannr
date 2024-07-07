dfc13e3539f02129a2dc6d68009d368fdca16bd3
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-07-08 05:07:16 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -39,8 +39,7 @@ public class Main {
     public static void main(String[] args) {
         assert (batteryIsOk(25, 70, 0.7f));
         assert (batteryIsOk(0, 20, 0.0f));
-        assert (batteryIsOk(45, 80, 0.8f));
-        
+        assert (batteryIsOk(45, 80, 0.7f));
         assert (!batteryIsOk(25, 10, 0.7f));
         assert (!batteryIsOk(25, 70, 0.9f));
         assert (!batteryIsOk(50, 85, 0.0f));
