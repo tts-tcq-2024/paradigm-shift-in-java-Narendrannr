d27a2279bdeddc5111c841fa28faf1e0adcc3f8f
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-08-24 11:06:50 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -34,13 +34,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        assert (batteryIsOk(25, 70, 0.7f));
+     assert (batteryIsOk(25, 70, 0.7f));
         assert (batteryIsOk(0, 20, 0.0f));
         assert (batteryIsOk(45, 80, 0.7f));
         assert (!batteryIsOk(25, 10, 0.7f));
         assert (!batteryIsOk(25, 70, 0.9f));
         assert (!batteryIsOk(50, 85, 0.0f));
-
+        assert (batteryIsOk(22, 70, 0.5f)); 
+        assert (batteryIsOk(78, 70, 0.5f)); 
+        assert (batteryIsOk(25, 70, 0.77f)); 
+        assert (batteryIsOk(42, 70, 0.5f)); 
+        
         currentLanguage = new GermanLanguage();
         assert (!batteryIsOk(-1, 70, 0.5f));
         assert (!batteryIsOk(25, 85, 0.5f));
