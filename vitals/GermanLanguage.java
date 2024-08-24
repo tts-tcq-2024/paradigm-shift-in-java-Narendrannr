073d026da96c27be3f9195ebddaf600d41cc55c8
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-08-24 11:02:48 +0530

    Update GermanLanguage.java

--- vitals/GermanLanguage.java
+++ vitals/GermanLanguage.java
@@ -1,21 +1,6 @@
 package vitals;
 
 public class GermanLanguage implements Language {
-    @Override
-    public String temperatureOutOfRange() {
-        return "Die Temperatur liegt außerhalb des Bereichs!";
-    }
-
-    @Override
-    public String socOutOfRange() {
-        return "Der Ladezustand liegt außerhalb des Bereichs!";
-    }
-
-    @Override
-    public String chargeRateOutOfRange() {
-        return "Die Ladegeschwindigkeit liegt außerhalb des Bereichs!";
-    }
-
     public String warningMessage(String parameterName, String condition) {
         return "Warnung: " + parameterName + " ist " + condition + "!";
     }
