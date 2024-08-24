769b49033178d1df002c177e96c1548dbc58f4a0
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-08-24 11:03:01 +0530

    Update EnglishLanguage.java

--- vitals/EnglishLanguage.java
+++ vitals/EnglishLanguage.java
@@ -1,21 +1,6 @@
 package vitals;
 
 public class EnglishLanguage implements Language {
-    @Override
-    public String temperatureOutOfRange() {
-        return "Temperature is out of range!";
-    }
-
-    @Override
-    public String socOutOfRange() {
-        return "State of Charge is out of range!";
-    }
-
-    @Override
-    public String chargeRateOutOfRange() {
-        return "Charge Rate is out of range!";
-    }
-
     public String warningMessage(String parameterName, String condition) {
         return "Warning: " + parameterName + " is " + condition + "!";
     }
